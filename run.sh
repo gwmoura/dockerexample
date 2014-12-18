449e7ea4f741d5157ac7d4f5409831e1204e41d1
George Moura <gwmoura@gmail.com>
2014-12-18 08:18:51 -0300

    Remove conditions

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #sudo docker build --no-cache -t george/nodejsapp .
 sudo docker build -t george/nodejsapp .
-sudo docker stop nodejsapp - if container is running
-sudo docker rm nodejsapp - if container is running
+sudo docker stop nodejsapp
+sudo docker rm nodejsapp
 sudo docker run -p 80:8080 --name nodejsapp -d george/nodejsapp
