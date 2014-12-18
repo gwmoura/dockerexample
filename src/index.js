4fe7e7b9dc2a622667bc9b80d8b24ba57651685b
George Moura <gwmoura@gmail.com>
2014-12-18 08:55:44 -0300

    Add query string

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ var PORT = 8080;
 // App
 var app = express();
 app.get('/', function (req, res) {
-  res.send('Hello World George\n');
+  res.send('Hello World George '+req.query.name+'\n');
 });
 
 app.listen(PORT);
